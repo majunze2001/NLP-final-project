3526c9856aabbc15979ef084c1096edf4e7aa2b7
Jeff Ma <jm9527@nyu.edu>
2023-04-10 13:32:15 -0400

    update nyt scraper for timeout

--- src/nyt_scraper/index.js
+++ src/nyt_scraper/index.js
@@ -26,26 +26,29 @@ const main = async () => {
                 const searchButtons = await page.$$('button.css-4d08fs[data-testid="search-multiselect-button"]');
                 await searchButtons[1].click();
                 const totalNum = await page.$eval('.css-17fq56o', element => element.textContent);
-                console.log(totalNum);
+                console.log(`${dateString} expected: `, totalNum);
                 await searchButtons[1].click();
 
-                await page.waitForTimeout(3000);
-                for (let i = 0; i < totalNum / 10; i++) {
+                let count = await page.$$eval('[data-testid="search-results"]', elements => elements.length);
+                while (count < totalNum) {
                     // Click the "Show More" button
                     const showMoreButton = await page.$('[data-testid="search-show-more-button"]');
                     if (showMoreButton) {
                         await showMoreButton.click();
-                        // Wait for new search results to load
-                        await page.waitForSelector('[data-testid="search-results"] li:last-child');
+                        // Wait for the search results to load
+                        await Promise.all([
+                            page.waitForSelector('[data-testid="search-results"] li:last-child'),
+                            page.waitForSelector('[data-testid="search-show-more-button"]'),
+                        ]);
+                    } else {
+                        console.log("showMoreButton is gone ... at ", dateString, count);
+                        break;
                     }
-                    await page.waitForTimeout(1000);
+                    count = await page.$$eval('[data-testid="search-results"]', elements => elements.length);
+                    await page.waitForTimeout(200);
                 }
 
                 // Extract the links
-                // const links = await page.$$eval(
-                //     '[data-testid="search-results"] li.css-1l4w6pd[data-testid="search-bodega-result"] div.css-1i8vfl5 div.css-e1lvw9 a',
-                //     (elements) => elements.map((element) => element.href)
-                // );
                 const links = await page.$$eval(
                     '.css-e1lvw9 a',
                     (elements) => elements.map((element) => element.href)
@@ -55,15 +58,6 @@ const main = async () => {
                 const matchingLinks = links.filter((link, index) => {
                     return link.startsWith(`https://www.nytimes.com/${dateString.slice(0, 4)}/${dateString.slice(4, 6)}/${dateString.slice(6)}/`) && index < totalNum
                 });
-
-                // Write the links to a file for this URL
-                const linksDir = path.join(__dirname, 'links');
-                if (!fs.existsSync(linksDir)) {
-                    fs.mkdirSync(linksDir);
-                }
-                const urlFileName = `${dateString}.txt`;
-                const urlFilePath = path.join(linksDir, urlFileName);
-                fs.writeFileSync(urlFilePath, matchingLinks.join('\n'));
 
                 await page.close()
                 return matchingLinks
@@ -81,19 +75,39 @@ const main = async () => {
     const dateStrings = [];
     for (let year = 2020; year <= 2021; year++) {
         const maxMonth = 12;
-        for (let month = 6; month <= maxMonth; month++) {
+        for (let month = 1; month <= maxMonth; month++) {
             const maxDay = new Date(year, month, 0).getDate(); // Get the number of days in this month
-            for (let day = 18; day <= maxDay; day++) {
+            for (let day = 1; day <= maxDay; day++) {
                 const dateString = `${year}${month.toString().padStart(2, '0')}${day.toString().padStart(2, '0')}`;
                 dateStrings.push(dateString);
             }
+        }
+    }
+    const updateLinks = async (dateString, links) => {
+        // Write the links to a file for this URL
+        const linksDir = path.join(__dirname, 'links');
+        if (!fs.existsSync(linksDir)) {
+            fs.mkdirSync(linksDir);
+        }
+
+        const urlFileName = `${dateString}.txt`;
+        const urlFilePath = path.join(linksDir, urlFileName);
+        if (fs.existsSync(urlFilePath)) {
+            const existingLinks = fs.readFileSync(urlFilePath, 'utf8').split('\n').filter(Boolean);
+            if (links.length > existingLinks.length) {
+                fs.writeFileSync(urlFilePath, links.join('\n'));
+                console.log(`Links for ${dateString}: ${existingLinks.length}->${links.length}`);
+            }
+        } else {
+            fs.writeFileSync(urlFilePath, links.join('\n'));
+            console.log(`Links for ${dateString}:`, links.length);
         }
     }
 
     // Call the scrapeLinks function for each dateString
     for (const dateString of dateStrings) {
         const links = await scrapeLinks(dateString);
-        console.log(`Links for ${dateString}:`, links.length);
+        await updateLinks(dateString, links)
     }
 
 
